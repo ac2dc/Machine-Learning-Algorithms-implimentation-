08e79b668b571856cab93d15c3cacfe5229741bf
ac2dc <bhaskar.jnvd@gmail.com>
2022-04-03 03:44:13 +0530

    init

---
+++ knn.py
@@ -0,0 +1,52 @@
+''' Classification using KNN algorithm using car evaluation data'''
+
+# IMPORTS
+
+from sklearn import preprocessing
+from sklearn.utils import shuffle
+from sklearn.neighbors import KNeighborsClassifier
+import pandas as pd
+import numpy as np
+from sklearn import linear_model, preprocessing
+from sklearn.model_selection import train_test_split
+
+# DATA
+data = pd.read_csv('datasets/car_evaluation.csv')
+# print(data.head())
+
+# Converting to int with label-encoding
+le = preprocessing.LabelEncoder()
+
+buying = le.fit_transform(list(data['buying']))
+maint = le.fit_transform(list(data['maint']))
+door = le.fit_transform(list(data['door']))
+persons = le.fit_transform(list(data['persons']))
+lug_boot = le.fit_transform(list(data['lug_boot']))
+safety = le.fit_transform(list(data['safety']))
+cls = le.fit_transform(list(data['class']))
+
+# SPLITTING DATASET 
+
+predict = 'class'
+
+x = list(zip(buying, maint, door, persons, lug_boot, safety))
+y = list(cls)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1)
+
+# TRAINNING
+
+model = KNeighborsClassifier(n_neighbors=9)
+
+model.fit(x_train, y_train)
+
+acc = model.score(x_test, y_test)
+print(acc)
+
+# PREDICTION
+predicted = model.predict(x_test)
+names = ['unacc', 'acc', 'good', 'vgood']
+
+for x in range(len(predicted)):
+	print("Predicted: ", names[predicted[x]], "Data: ", x_test[x], "Actual: ", names[y_test[x]])
+	n = model.kneighbors([x_test[x]], 9, True)
+print("N: ", n)
